f08e4a8a26c7d08201b1ad9e2be23e0b415a4701
Mysterium422 <mysterium422@gmail.com>
2024-11-12 17:46:23 -0500

    Adds Detector Limelight

--- src/main/java/frc/robot/sensors/Limelight/LimelightIO/LimelightIODetector.java
+++ src/main/java/frc/robot/sensors/Limelight/LimelightIO/LimelightIODetector.java
@@ -1,13 +1,18 @@
 package frc.robot.sensors.Limelight.LimelightIO;
 
 import frc.lib.LimelightHelpers.LimelightTarget_Detector;
+import frc.lib.LimelightHelpers.LimelightTarget_Fiducial;
 
 public class LimelightIODetector extends LimelightIO {
     public LimelightIODetector(String name) {
         super(name);
     }
     
-    public LimelightTarget_Detector[] getResults() {
+    public LimelightTarget_Detector[] getTargets() {
         return getRawResults().targets_Detector;
+    }
+
+    public LimelightTarget_Detector getTarget() {
+        return getTargets()[0];
     }
 }
--- src/main/java/frc/robot/sensors/Limelight/LimelightShooter.java
+++ src/main/java/frc/robot/sensors/Limelight/LimelightShooter.java
@@ -1,10 +1,15 @@
 package frc.robot.sensors.Limelight;
 
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import frc.lib.VirtualSubsystem;
 import frc.robot.FieldConstants.Tags;
 import frc.robot.sensors.Limelight.LimelightIO.LimelightIOFiducials;
+import lombok.Getter;
+import lombok.extern.java.Log;
 
 public class LimelightShooter extends VirtualSubsystem {
     private final LimelightIOFiducials limelightIO;
@@ -18,18 +23,19 @@ public class LimelightShooter extends VirtualSubsystem {
 
     }
 
+    @AutoLogOutput(key = "{name}/hasSpeakerTarget")
     public boolean hasSpeakerTarget() {
         return limelightIO.hasTarget(Tags.SPEAKER_CENTER.getId());
     }
 
+    @AutoLogOutput(key = "{name}/horizontalOffsetToSpeaker")
     public Angle getHorizontalOffsetToSpeaker() {
         return limelightIO.getHorizontalOffset(Tags.SPEAKER_CENTER.getId());
     }
 
+    @AutoLogOutput(key = "{name}/distanceToSpeaker")
     public Distance getDistanceToSpeaker() {
         return limelightIO.getDistance(Tags.SPEAKER_CENTER.getId());
     }
-
-
 
 }
